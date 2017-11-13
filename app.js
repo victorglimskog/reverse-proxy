82b1071f61027d5b098e27b1fff57fd61ec1f8a0
Victor Glimskog <victor.glimskog@gmail.com>
2017-11-13 14:50:51 +0100

    error in proxy

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const httpProxy = require('http-proxy');
 // Create a new reverse proxy
 const proxy = httpProxy();
 
+proxy.on('error', (e) => {
+    console.log('I AM ERROR')
+});
+
 // Create a new webserver
 http.createServer((req,res) => {
     res.write('url: ', req.url);
