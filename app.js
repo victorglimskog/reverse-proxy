4432b75acb3a553602d122268c797d389b072aa3
Victor Glimskog <victor.glimskog@gmail.com>
2017-11-13 15:22:09 +0100

    proxy

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ const httpProxy = require('http-proxy');
 // Create a new reverse proxy
 const proxy = httpProxy.createProxyServer();
 
-// proxy.on('error', (e) => {
-//     console.log('I AM ERROR')
-// });
-
 // Create a new webserver
 http.createServer((req,res) => {
     const host = req.headers.host;
-    res.end(host + req.url);
+    const hostParts = host.split('.');
+    const topDomain = hostParts.pop();
+    const domain = hostParts.pop();
+    const subDomain = hostParts.join('.');
+    const urlParts = req.url.split('/');
+
+    let port;
+    if (subDomain = '' || subDomain == 'www') {
+        port = 4000;
+    } else if (subDomain = 'me') {
+        port = 3000;
+    } else {
+        res.statusCode = 500;
+        res.end('Can not find your app!');
+    }
+
+    if (port) {
+        proxy.web({target: '127.0.0.1:' + port});
+    }
 }).listen(80);
