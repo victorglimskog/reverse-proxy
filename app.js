4a9d66fbb88c62b49bb0c2a8806b0fc0ea67209b
Victor Glimskog <victor.glimskog@gmail.com>
2017-11-16 11:55:19 +0100

    added port for cert-bot-helper

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ proxy.on('error', function(e) {
 
 // Create a new webserver
 http.createServer((req,res) => {
+    // replace setResponseHeaders
+    setResponseHeaders(req,res);
+
     const host = req.headers.host;
     const hostParts = host.split('.');
     const topDomain = hostParts.pop();
@@ -20,17 +23,26 @@ http.createServer((req,res) => {
 
     let port;
     let subDomain = hostParts.join('.');
-
-    if (subDomain == '' || subDomain == 'www') {
-        port = 4000;
+    if (urlParts[0] === '.well-known') {
+        port = 5000; // app: cert-bot-helper
+    } else if (subDomain == '' || subDomain == 'www') {
+        port = 4000; // app: testapp
     } else if (subDomain == 'me') {
-        port = 3000;
+        port = 3000; // app: small-node
     } else {
-        res.statusCode = 500;
-        res.end('Can not find your app!');
+        res.statusCode = 404;
+        res.end('This is not the page you are looking for.');
     }
 
     if (port) {
         proxy.web(req, res, {target: 'http://127.0.0.1:' + port});
     }
 }).listen(80);
+
+function setResponseHeaders(req,res) {
+    res.oldWriteHead = res.writeHead;
+    res.writeHead = function (statusCode, headers) {
+        res.setHeader('x-powered-by','vgl server');
+        res.oldWriteHead(statusCode,headers);
+    }
+}
